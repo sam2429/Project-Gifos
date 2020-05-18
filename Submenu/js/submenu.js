3a0b6fd7363f5f9f08f5766ed4a57efce5e3bce6
sergilab-bit <smartinez3536@gmail.com>
2020-05-17 22:19:08 -0300

    Se sube submenu

---
+++ Submenu/js/submenu.js
@@ -0,0 +1,45 @@
+//document.body.onload = addElement;
+
+let ctnmenusecsec = 'ctn-menu-sec-sec';
+let menusecsec = 'menu-sec-sec';
+//let ctnmenuprisec = 'ctn-menu-pri-sec';
+
+//const listSailor = ['Sailor Day','Sailor Nigth'];
+const menunav = document.createElement ('nav');
+const meul = document.createElement ('ul');
+const li1= document.createElement ('li');
+const li2= document.createElement ('li');
+const button1 = document.createElement('button')
+const button2 = document.createElement('button');
+
+
+menunav.classList = ctnmenusecsec;
+menunav.id = 'ctn-menu-pri-sec';
+
+meul.classList = menusecsec;
+
+
+menunav.appendChild(meul);
+meul.appendChild(li1);
+meul.appendChild(li2);
+
+li1.appendChild(button1);
+li2.appendChild(button2);
+
+button1.type = 'button';
+button1.classList = 'rectangular rect-day'; 
+button1.id = 'day';
+button1.value = 'day';
+//button1.onclick ="sailorDay()";
+button1.innerHTML = '<u>S</u>ailor Day';
+
+button2.type = 'button';
+button2.classList = 'rectangular rect-day'; 
+button2.id = 'nigth';
+button2.value ='nigth';
+//button2.GlobalEventHandlers.onclick ="sailorNigth()";
+button2.innerHTML = '<u>S</u>ailor Nigth';
+
+
+document.body.appendChild(menunav)
+
